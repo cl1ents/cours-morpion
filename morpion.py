b4d567576df0c56cf1198138efa6f69ecf3b2d02
evⁿ <gh@cli.lol>
2022-11-15 13:08:53 +0100

    Basic morpion finished!

--- morpion.py
+++ morpion.py
@@ -1,32 +1,39 @@
 # FUNCTIONS
 
+from os import system, name as osname
+"""
+Clears console
+"""
+def clear():
+    system('cls' if osname=='nt' else 'clear')
+
 """
 Creates a 3 by 3 grid filled with 0s
 """
 def createGrid(): # [y][x]
     return [
-        [1,0,0],
-        [0,2,0],
-        [0,0,2]
+        [0,0,0],
+        [0,0,0],
+        [0,0,0]
     ]
 
 """
 Prints the grid in a user-friendly way
 e.g:
-    1 2 3
+    A B C
     _ _ _
-A | X O O
-B | O X X
-C | X O -
+1 | X O O
+2 | O X X
+3 | X O -
 """
 def printGrid(grid):
-    print("    1 2 3")
+    print("    A B C")
     print("    _ _ _")
     for i in range(len(grid)):
-        print("ABC"[i], end=" | ")
+        print("123"[i], end=" | ")
         row = grid[i]
         for e in row:
-            print("X" if e == 1 else "O" if e == 2 else ".", end = " ")
+            print(".XO"[e], end = " ")
         print("")
 
 def testCoordinate(grid, x, y):
@@ -42,13 +49,27 @@ def setValue(grid, x, y, value):
     return True
 
 def toCoordinates(coord):
-    if len(coord) == 2:
-        if coord[0] in "ABC" and coord[1] in "123":
-            return ("ABC".index(coord[0]), "123".index(coord[1]))
-        elif coord[1] in "ABC" and coord[0] in "123":
-            return ("ABC".index(coord[1]), "123".index(coord[0]))
+    coordLower = coord.lower()
+    if len(coordLower) == 2: 
+        if coordLower[0] in "abc" and coordLower[1] in "123":
+            return ("abc".index(coordLower[0]), "123".index(coordLower[1]))
+        elif coordLower[1] in "abc" and coordLower[0] in "123":
+            return ("abc".index(coordLower[1]), "123".index(coordLower[0]))
     
     return (-1, -1)
+
+"""
+Custom input to intercept whatever the player types.
+(Handles exit)
+"""
+def customInput(txt):
+    while True:
+        enteredInput = input(txt)
+        if enteredInput.lower() == "exit":
+            if input("Are you sure? (yes/no) > ").lower() == "yes":
+                exit()
+        else:
+            return enteredInput
 
 def isWinner(grid, player):
     for i in range(3):
@@ -70,8 +91,44 @@ def isWinner(grid, player):
                 return True
     return False
 
+def round(grid, player):
+    coordinates = (-1, -1)
+    while coordinates == (-1, -1):
+        clear()
+        printGrid(grid)
+        print("\nITS PLAYER "+str(player)+"'s TURN! ("+".XO"[player]+")")
+        playerInput = customInput("Enter coordinates (e.g: A2) > ")
+        coordinates = toCoordinates(playerInput)
+
+        if getValue(grid, coordinates[0], coordinates[1]) != 0:
+            coordinates = (-1, -1)
+    
+    setValue(grid, coordinates[0], coordinates[1], player)
+    
+clear()
+print("""----
+Welcome to MORPION!!
+----
+Le but du jeu est simple:
+Il suffit d'aligner, avant son adversaire, 3 symboles identiques horizontalement, verticalement ou en diagonale sur une grille de 3x3.
+Le joueur ayant aligné 3 symboles gagne la partie.
+Le joueur 1 a le symbole X et le joueur 2 a le symbole O
+Pour jouer, le joueur indique les coordonnées de la case dans laquelle il veut jouer, par exemple "C2"
+
+""")
+input('(Appuyez sur entrer pour continuer.)')
 
 grid = createGrid()
-printGrid(grid)
-print(isWinner(grid, 1))
-print(toCoordinates('A3'))
+turn = 2
+playing = True
+
+while playing:
+    turn = (turn%2)+1
+    round(grid, turn)
+    
+    if isWinner(grid, turn):
+        clear()
+        printGrid(grid)
+        print("\nPLAYER "+str(turn)+" WINS!\n")
+        input('(Appuyez sur entrer pour continuer.)')
+        playing = False
